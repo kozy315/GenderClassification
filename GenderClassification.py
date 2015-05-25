73dd22ade2f953ea21bce0c1b2d0ceaeacc25d21
Jacob <jacob_kozol@yahoo.com>
2015-05-25 14:45:22 -0400

    Fixed comments and training

--- GenderClassification.py
+++ GenderClassification.py
@@ -1,22 +1,21 @@
 from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
-from features import mfcc, logfbank
+from features import mfcc
 from scipy.io import wavfile
 from pybrain.datasets import SupervisedDataSet as SDS
 from pybrain.supervised.trainers import BackpropTrainer
 
-#reads wav file and transforms it
-datatset = SDS(13, 1)
-for i in range(10)
+#reads wav file, transforms it, and turns it into a data set
+datatset = SDS(13, 1) #creates the dataset
+for i in range(10):
 	rate, data = wavfile.read("audio%d.wav" % i)
 	mfcc_feat = mfcc(data,rate)
 	datachunk = mfcc_feat.mean(axis=0) #averages all of the feature vectors
+	#gender alternates every other file so this just sets what the target result is.
 	if i % 2 == 0:
 		target = "0"
 	else:
 		target = "1"
-	dataset.addSample(datachunk, target);
-
-#sets up data
+	dataset.addSample(datachunk, target); #adds the mfcc features to the dataset
 
 #creates the FFN 
 net = FeedForwardNetwork()
@@ -43,13 +42,16 @@ net.sortModules()
 
 #sets up the trainer
 trainer = BackpropTrainer(net, dataset)
-#taken from pybrain tutorial to train and show errors
-for i in range (20):
-	trnresult = percentError( trainer.testOnClassData(), 
-                              trndata['class'] )
-    tstresult = percentError( trainer.testOnClassData(
-           dataset=tstdata ), tstdata['class'] )
 
-    print "epoch: %4d" % trainer.totalepochs, \
-          "  train error: %5.2f%%" % trnresult, \
-          "  test error: %5.2f%%" % tstresult
+#simple train
+#trainer.trainEpoch(5)
+
+#more detailed train taken from pybrain tutorial to train and show errors
+for i in range (20):
+	trainer.trainEpochs(5)
+	trnresult = percentError(trainer.testOnClassData(), trndata['class'])
+	tstresult = percentError(trainer.testOnClassData(dataset=tstdata), tstdata['class'])
+	
+	print "epoch: %4d" % trainer.totalepochs,\
+		"  train error: %5.2f%%" % trnresult,\
+		"  test error: %5.2f%%" % tstresult
