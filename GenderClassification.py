3b91911e45211d74d6f9c9b2d7058ac91b8f9521
Jacob <jacob_kozol@yahoo.com>
2015-06-03 23:08:27 -0400

    Made it work

--- GenderClassification.py
+++ GenderClassification.py
@@ -3,9 +3,11 @@ from features import mfcc
 from scipy.io import wavfile
 from pybrain.datasets import SupervisedDataSet as SDS
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.tools.validation import Validator
 
 #reads wav file, transforms it, and turns it into a data set
-datatset = SDS(13, 1) #creates the dataset
+ds = SDS(13, 1) #creates the dataset
+
 for i in range(10):
 	rate, data = wavfile.read("audio%d.wav" % i)
 	mfcc_feat = mfcc(data,rate)
@@ -15,8 +17,21 @@ for i in range(10):
 		target = "0"
 	else:
 		target = "1"
-	dataset.addSample(datachunk, target); #adds the mfcc features to the dataset
+	ds.addSample(datachunk, target); #adds the mfcc features to the dataset
+	
+dsTest = SDS(13, 1)
 
+for i in range(2):
+	rate, data = wavfile.read("audioTest%d.wav" % i)
+	mfcc_feat = mfcc(data,rate)
+	datachunk = mfcc_feat.mean(axis=0) #averages all of the feature vectors
+	#gender alternates every other file so this just sets what the target result is.
+	if i % 2 == 0:
+		target = "0"
+	else:
+		target = "1"
+	dsTest.addSample(datachunk, target); #adds the mfcc features to the dataset
+	
 #creates the FFN 
 net = FeedForwardNetwork()
 
@@ -41,17 +56,8 @@ net.addConnection(outputConnect)
 net.sortModules()
 
 #sets up the trainer
-trainer = BackpropTrainer(net, dataset)
+trainer = BackpropTrainer(net, ds)
 
 #simple train
-#trainer.trainEpoch(5)
-
-#more detailed train taken from pybrain tutorial to train and show errors
-for i in range (20):
-	trainer.trainEpochs(5)
-	trnresult = percentError(trainer.testOnClassData(), trndata['class'])
-	tstresult = percentError(trainer.testOnClassData(dataset=tstdata), tstdata['class'])
-	
-	print "epoch: %4d" % trainer.totalepochs,\
-		"  train error: %5.2f%%" % trnresult,\
-		"  test error: %5.2f%%" % tstresult
+trainer.trainEpochs(50)
+trainer.testOnData(dataset=dsTest, verbose=True)
